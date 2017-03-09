0bb7888e62fd7d9417264e689dfa2b6788f311b2
Andrew <andrew.vinocur@gmail.com>
2017-03-09 10:38:08 -0800

    modifies ride component and html to use data from rails server

--- src/app/ride/ride.component.ts
+++ src/app/ride/ride.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-ride',
@@ -6,19 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./ride.component.css']
 })
 export class RideComponent {
-  ridesAndTimes = [
-    {ride: 'Astro Orbitor', time: 30 } ,
-    {ride: 'Autotopia', time: 45},
-    {ride: 'Buzz Lightyear Astro Blasters', time: 25},
-    {ride: 'Finding Nemo Submarine Voyage', time: 15},
-    {ride: 'Monorail', time: 20},
-    {ride: 'Space Mountain', time: 65},
-    {ride: 'Star Tours', time: 45},
-    {ride: 'Star Wars Launch Bay', time: 30},
-    {ride: 'Starcade', time: 5}
-  ]
+  rides;
 
-  constructor() { }
-
+  constructor(private http:Http ) {
+    http.get('http://localhost:3000/rides.json')
+        .subscribe(res => this.rides = res.json());
+  }
 
 }
